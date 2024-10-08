edc9730592fe4819b51f9e815bc888ae79a516b7
sukku-akky <somasukku@gmail.com>
2024-10-09 00:36:31 +0530

    deployment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React,{useContext} from "react";
+import React,{useContext,Suspense,lazy} from "react";
 import { BrowserRouter as Router, Route, Routes ,useLocation,Navigate}from 'react-router-dom';
-import Home from "./pages/Home";
-import Store from "./pages/Store";
+//import Home from "./pages/Home";
+//import Store from "./pages/Store";
 import Header from "./components/Header/Header";
 import MyNavbar from "./components/Navbar/MyNavbar"
 import Footer from "./components/Footer/Footer";  
-import About from "./pages/About";
+//import About from "./pages/About";
 import CartProvider from "./store/CartProvider";
-import ContactPage from "./pages/ContactPage"
-import SingleProductPage from "./pages/ProductPage/SingleProductPage";
-import AuthPage from "./pages/ProductPage/AuthPage";
-import ProfilePage from "./pages/ProductPage/ProfilePage";
+//import ContactPage from "./pages/ContactPage"
+//import SingleProductPage from "./pages/ProductPage/SingleProductPage";
+//import AuthPage from "./pages/ProductPage/AuthPage";
+//import ProfilePage from "./pages/ProductPage/ProfilePage";
 import AuthContext from "./store/auth-context";
+
+
+const Home = lazy(() => import("./pages/Home"));
+const Store = lazy(() => import("./pages/Store"));
+const About = lazy(() => import("./pages/About"));
+const ContactPage = lazy(() => import("./pages/ContactPage"));
+const SingleProductPage = lazy(() => import("./pages/ProductPage/SingleProductPage"));
+const AuthPage = lazy(() => import("./pages/ProductPage/AuthPage"));
+const ProfilePage = lazy(() => import("./pages/ProductPage/ProfilePage"));
 
 
 function App() {
@@ -19,9 +28,10 @@ function App() {
   
   return (
     <CartProvider>
+      
     <Router>
       <MyNavbar/>
-      
+      < Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/"  element={<Home/>}/>
 
@@ -37,9 +47,10 @@ function App() {
         
         <Route path="/products/:id" element={<SingleProductPage/>}/>
       </Routes>
-      
+      </Suspense>
       <Footer />
     </Router>
+    
   </CartProvider>
   );
 }
